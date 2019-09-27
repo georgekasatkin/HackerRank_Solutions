16877c96ded93b6b80a0bd3674c338090c4368db
George Kasatkin <george227@outlook.com>
2019-09-27 13:11:19 +0500

    Changed directory structure. Added description files. Added RansomNote

---
+++ Interview_Preparation_Kit/Dictionaries_and_Hashmaps/Ransom_Note/RansomeNote.Tests/SolutionTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RansomNote
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        static object[] testCases = 
+        {
+            new object[]
+            {
+            new string[] {"give", "me", "one", "grand", "today", "night"},
+            new string[] {"give", "one", "grand", "today"},
+            "Yes"
+            },
+            new object[]
+            {
+            new string[] {"h", "ghq", "g", "xxy", "wdnr", "anjst", "xxy", "wdnr", "h", "h", "anjst", "wdnr"},
+            new string[] {"h", "ghq"},
+            "Yes"
+            },
+            new object[]
+            {
+            new string[] {"ive", "got", "a", "lovely", "bunch", "of", "coconuts"},
+            new string[] {"ive", "got", "some", "coconuts"},
+            "No"
+            },
+            new object[]
+            {
+            new string[] {"two", "times", "three", "is", "not", "four"},
+            new string[] {"two", "times", "two", "is", "four"},
+            "No"
+            }
+        };
+        [Test, TestCaseSource("testCases"), MaxTime(50)]
+        public void isValidNote_GoodCases_ReturnsYes(string[] magazine, string[] note, string expectedResult)
+        {
+            string result = Program.canCreateNoteFromMagazine(magazine, note);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Interview_Preparation_Kit/Dictionaries_and_Hashmaps/Ransom_Note/RansomeNote/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RansomNote
+{
+    public class Program
+    {
+        public static string canCreateNoteFromMagazine(string[] magazine, string[] note)
+        {
+            Dictionary<string, int> magazineDictionary = new Dictionary<string, int>();
+            foreach (string s in magazine)
+            {
+                if (magazineDictionary.ContainsKey(s))
+                    magazineDictionary[s]++;
+                else
+                    magazineDictionary.Add(s, 1);
+            }
+
+            bool validNote = true;
+            foreach (string s in note)
+            {
+                if (!magazineDictionary.ContainsKey(s) || --magazineDictionary[s] < 0)
+                {
+                    validNote = false;
+                    return "No";
+                }
+            }
+            return validNote ? "Yes" : "No";
+        }
+        static void Main(string[] args)
+        {
+            string[] magazine = Array.ConvertAll(Console.ReadLine().Split(' '), tmp => tmp);
+            string[] note = Array.ConvertAll(Console.ReadLine().Split(' '), tmp => tmp);
+            string result = canCreateNoteFromMagazine(magazine, note);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}

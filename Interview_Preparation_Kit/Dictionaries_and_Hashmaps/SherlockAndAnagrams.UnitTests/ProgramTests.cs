240aae12c286442da54c69a1d1d3450535b89949
George Kasatkin <george227@outlook.com>
2019-09-27 10:17:09 +0500

    Added a solution for SherlockAndAnagrams

---
+++ Interview_Preparation_Kit/Dictionaries_and_Hashmaps/SherlockAndAnagrams.UnitTests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SherlockAndAnagrams
+{
+    [TestFixture]
+    class ProgramTests
+    {
+        static object[] anagramCases = 
+        {
+            new object [] {"abba", 4},
+            new object[] {"abcd",0},
+            new object[] {"ifailuhkqq",3},
+            new object[] {"kkkk",10},
+            new object[] {"ifailuhkqqhucpoltgtyovarjsnrbfpvmupwjjjfiwwhrlkpekxxnebfrwibylcvkfealgonjkzwlyfhhkefuvgndgdnbelgruel", 399},
+            new object[] {"gffryqktmwocejbxfidpjfgrrkpowoxwggxaknmltjcpazgtnakcfcogzatyskqjyorcftwxjrtgayvllutrjxpbzggjxbmxpnde", 471},
+            new object[] {"mqmtjwxaaaxklheghvqcyhaaegtlyntxmoluqlzvuzgkwhkkfpwarkckansgabfclzgnumdrojexnrdunivxqjzfbzsodycnsnmw",370},
+            new object[] {"ofeqjnqnxwidhbuxxhfwargwkikjqwyghpsygjxyrarcoacwnhxyqlrviikfuiuotifznqmzpjrxycnqktkryutpqvbgbgthfges",403},
+            new object[] {"zjekimenscyiamnwlpxytkndjsygifmqlqibxxqlauxamfviftquntvkwppxrzuncyenacfivtigvfsadtlytzymuwvpntngkyhw",428},
+            new object[] {"dfcaabeaeeabfffcdbbfaffadcacdeeabcadabfdefcfcbbacadaeafcfceeedacbafdebbffcecdbfebdbfdbdecbfbadddbcec", 2452},
+            new object[] {"bcgdehhbcefeeadchgaheddegbiihehcbbdffiiiifgibhfbchffcaiabbbcceabehhiffggghbafabbaaebgediafabafdicdhg", 1305},
+            new object[] {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",166650}
+        };
+
+        [Test, TestCaseSource("anagramCases"), MaxTime(100)]
+        public void countAnagrams_anagramsCases_returnsTrue(string source, int expectedResult)
+        {            
+            int result = Program.countAnagrams(source);            
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Interview_Preparation_Kit/Dictionaries_and_Hashmaps/SherlockAndAnagrams/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SherlockAndAnagrams
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = Console.ReadLine();
+            int result = countAnagrams(source);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }        
+        public static int countAnagrams(string s)
+        {
+            var substringsDict = new Dictionary<string, int>();
+            int sum = 0;
+            for (int start = 0; start < s.Length; start++)
+            {
+                for (int tail = 1; tail < s.Length - start + 1; tail++)
+                {
+                    string subs = s.Substring(start, tail);
+                    char[] subsCharArray = subs.ToCharArray();
+                    Array.Sort(subsCharArray);
+                    subs = new String(subsCharArray);
+                    if (!substringsDict.ContainsKey(subs))
+                        substringsDict.Add(subs, 1);
+                    else
+                        substringsDict[subs]++;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> kvp in substringsDict)
+            {
+                if (kvp.Value > 1)
+                    for (int k = 1; k < kvp.Value; k++)
+                        sum += k;
+            }
+            return sum;
+        }
+    }
+}

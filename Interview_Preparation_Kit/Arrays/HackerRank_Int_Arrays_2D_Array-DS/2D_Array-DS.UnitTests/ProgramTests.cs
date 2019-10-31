e57a8f9f89f2deef2bc06e2dab37f56338fdad4b
George Kasatkin <georgekasatkin88@gmail.com>
2019-10-31 16:25:46 +0500

    Added 2D_Array-DS

---
+++ Interview_Preparation_Kit/Arrays/HackerRank_Int_Arrays_2D_Array-DS/2D_Array-DS.UnitTests/ProgramTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace _2D_Array_DS
+{
+    [TestFixture]
+    class ProgramTests
+    { 
+        static object[] arrayCases = 
+        {
+            new object[]
+            {
+                new int[6][] 
+                {
+                    new int[] { 1, 1, 1, 0, 0, 0 }, 
+                    new int[] { 0, 1, 0, 0, 0, 0 },
+                    new int[] { 1, 1, 1, 0, 0, 0 },
+                    new int[] { 0, 0, 2, 4, 4, 0 },
+                    new int[] { 0, 0, 0, 2, 0, 0 },
+                    new int[] { 0, 0, 1, 2, 4, 0 }
+                },
+            19
+            },
+            new object[]
+            {
+                new int[6][] 
+                {
+                    new int[] { -9, -9, -9,  1, 1, 1 }, 
+                    new int[] {  0, -9,  0,  4, 3, 2 },
+                    new int[] { -9, -9, -9,  1, 2, 3 },
+                    new int[] {  0,  0,  8,  6, 6, 0 },
+                    new int[] {  0,  0,  0, -2, 0, 0 },
+                    new int[] {  0,  0,  1,  2, 4, 0 }
+                },
+            28
+            },
+            new object[]
+            {
+                new int[6][] 
+                {
+                    new int[] { 1, 1,  1,  0,  0, 0 }, 
+                    new int[] { 0, 1,  0,  0,  0, 0 },
+                    new int[] { 1, 1,  1,  0,  0, 0 },
+                    new int[] { 0, 9,  2, -4, -4, 0 },
+                    new int[] { 0, 0,  0, -2,  0, 0 },
+                    new int[] { 0, 0, -1, -2, -4, 0 }
+                },
+            13
+            }
+        };
+        [Test, TestCaseSource("arrayCases"), MaxTime(100)]
+        public void countMaxHourglassSum_arrayCases_returnsTrue(int[][] source, int expectedResult)
+        {
+            int result = Program.countMaxHourglassSum(source);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
---
+++ Interview_Preparation_Kit/Arrays/HackerRank_Int_Arrays_2D_Array-DS/HackerRank_Int_Arrays_2D_Array-DS/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2D_Array_DS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {  
+            int[][] arr = new int[6][];
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+            int result = countMaxHourglassSum(arr);
+            Console.WriteLine(result);
+            Console.ReadLine();            
+        }
+        public static int countMaxHourglassSum(int[][] arr)
+        {
+            int maxSum = int.MinValue;
+            int currentSum = 0;
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    currentSum = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        currentSum += arr[x][y + i];
+                        currentSum += arr[x + 2][y + i];
+                    }
+                    currentSum += arr[x + 1][y + 1];
+                    if (currentSum > maxSum)
+                        maxSum = currentSum;
+                }
+            }            
+            return maxSum;
+        }
+    }
+}

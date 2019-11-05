c519a5c6750a4162634a3bc837acf2629f1f31d4
George Kasatkin <georgekasatkin88@gmail.com>
2019-11-05 14:07:12 +0500

    Updated 2D_Array-DS. Added Minimum_Swaps_2

---
+++ Interview_Preparation_Kit/Arrays/Minimum_Swaps_2/Minimum_Swaps_2.UnitTests/ProgramTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Minimum_Swaps_2
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        static object[] arrayCases = 
+        {
+            new object[] 
+            {
+            new int[] {4,3,1,2}, 
+            3
+            },
+            new object[] 
+            {
+            new int[] {2,3,4,1,5}, 
+            3
+            },
+            new object[] 
+            {
+            new int[] {1,3,5,2,4,6,7}, 
+            3
+            }
+        };
+
+        [Test, TestCaseSource("arrayCases"), MaxTime(100)]
+        public void countMinimumSwaps_arrayCases_returnsTrue(int[] source, int expectedResult)
+        {
+            int result = Program.minSwaps(source);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Interview_Preparation_Kit/Arrays/Minimum_Swaps_2/Minimum_Swaps_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minimum_Swaps_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int count = minSwaps(arr);
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+        static void swap(int a, int b, ref int[] arr)
+        {
+            int tmp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tmp;
+        }
+        public static int minSwaps(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != i + 1)
+                {
+                    swap(i, arr[i] - 1, ref arr);
+                    count++;
+                    i--;
+                }
+            }
+            return count;
+        }
+    }
+}

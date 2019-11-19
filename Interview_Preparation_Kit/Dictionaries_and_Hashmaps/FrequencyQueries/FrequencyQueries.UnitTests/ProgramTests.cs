ee08ac6c9c7cc97213c1a839b38cba7afb670958
George Kasatkin <georgekasatkin88@gmail.com>
2019-11-19 16:42:39 +0500

    Added FrequencyQueries

---
+++ Interview_Preparation_Kit/Dictionaries_and_Hashmaps/FrequencyQueries/FrequencyQueries.UnitTests/ProgramTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FrequencyQueries
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        static object[] queryCases = 
+        {
+            new object[]
+            {
+                new List<List<int>>() 
+                { 
+                new List<int> { 1, 1 }, 
+                new List<int> { 2, 2 },
+                new List<int> { 3, 2 },
+                new List<int> { 1, 1 },
+                new List<int> { 1, 1 },
+                new List<int> { 2, 1 },
+                new List<int> { 3, 2 }
+                },
+                new List<int>() { 0, 1 }
+            },
+            new object[]
+            {
+                new List<List<int>>() 
+                { 
+                new List<int> { 1, 5 }, 
+                new List<int> { 1, 6 },
+                new List<int> { 3, 2 },
+                new List<int> { 1, 10 },
+                new List<int> { 1, 10 },
+                new List<int> { 1, 6 },
+                new List<int> { 2, 5 },
+                new List<int> { 3, 2 }
+                },
+                new List<int>() { 0, 1 }
+            },
+            new object[]
+            {
+                new List<List<int>>() 
+                { 
+                new List<int> { 3, 4 }, 
+                new List<int> { 2, 1003 },
+                new List<int> { 1, 16 },
+                new List<int> { 3, 1 }                
+                },
+                new List<int>() { 0, 1 }
+            },
+            new object[]
+            {
+                new List<List<int>>() 
+                { 
+                new List<int> { 1, 3 }, 
+                new List<int> { 2, 3 },
+                new List<int> { 3, 2 },
+                new List<int> { 1, 4 },  
+                new List<int> { 1, 5 },  
+                new List<int> { 1, 5 },  
+                new List<int> { 1, 4 },  
+                new List<int> { 3, 2 },  
+                new List<int> { 2, 4 },
+                new List<int> { 3, 2 }  
+                },
+                new List<int>() { 0, 1, 1 }
+            }
+        };
+
+        [TestCaseSource("queryCases"), MaxTime(50)]
+        public static void ShowQueriesResults_queryCases(List<List<int>> source, List<int> expectedResult)
+        {
+            List<int> result = Program.FreqQuery(source);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Interview_Preparation_Kit/Dictionaries_and_Hashmaps/FrequencyQueries/FrequencyQueries/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrequencyQueries
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine().Trim());            
+            List<List<int>> queries = new List<List<int>>();
+            for (int i = 0; i < q; i++)
+            {
+                queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+            }
+            List<int> ans = FreqQuery(queries);
+            Console.WriteLine(String.Join("\n", ans));
+            Console.ReadLine();
+        }
+        public static List<int> FreqQuery(List<List<int>> queries)
+        {
+            int l = queries.Count;
+            Dictionary<int, int> dict = new Dictionary<int, int>(l);
+            Dictionary<int, int> frequencies = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+            for (int i = 0; i < l; i++)
+            {
+                switch (queries[i][0])
+                {
+                    case 1:
+                        {
+                            if (dict.ContainsKey(queries[i][1]))
+                                dict[queries[i][1]]++;
+                            else
+                                dict.Add(queries[i][1], 1);
+                            EditFrequencies(-1, dict[queries[i][1]], ref frequencies);
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (dict.ContainsKey(queries[i][1]) && dict[queries[i][1]] > 0)
+                            {
+                                dict[queries[i][1]]--;
+                                EditFrequencies(1, dict[queries[i][1]], ref frequencies);
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            if (frequencies.ContainsKey(queries[i][1]) && frequencies[queries[i][1]] > 0)
+                                result.Add(1);
+                            else
+                                result.Add(0);
+                            break;
+                        }
+                }
+            }
+            return result;
+        }
+        public static void EditFrequencies(int modifier, int dictCurrentElement, ref Dictionary<int, int> frequencies)
+        {
+            if (frequencies.ContainsKey(dictCurrentElement))
+                frequencies[dictCurrentElement]++;
+            else
+                frequencies.Add(dictCurrentElement, 1);
+
+            if (frequencies.ContainsKey(dictCurrentElement + modifier))
+                frequencies[dictCurrentElement + modifier]--;
+            else
+                frequencies.Add(dictCurrentElement + modifier, 1);
+        }
+    }
+}

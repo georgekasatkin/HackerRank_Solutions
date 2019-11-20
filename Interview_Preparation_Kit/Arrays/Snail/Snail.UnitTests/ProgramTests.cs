71f04662d176891fb00dd1440cdbed85604a86dd
George Kasatkin <georgekasatkin88@gmail.com>
2019-11-20 12:49:42 +0500

    Added Snail traverse

---
+++ Interview_Preparation_Kit/Arrays/Snail/Snail.UnitTests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Snail
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        static object[] arrayCases = 
+        {
+            new object[] 
+            {
+                new int[][]  
+                {
+                new []{1, 2, 3, 44, 5},
+                new []{4, 5, 6, 66, 8},
+                new []{7, 8, 9, 99, 9},
+                new []{10, 11, 12, 1212, 0}           
+                },
+                new int[] {1,2,3,44,5,8,9,0,1212,12,11,10,7,4,5,6,66,99,9,8}
+            }
+        };
+
+        [TestCaseSource("arrayCases"), MaxTime(50)]
+        public static void CalculateSnailPath_arrayCases(int[][] source, int[] expectedResult)
+        {
+            int[] result = Program.Snail(source);
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+    }
+}
---
+++ Interview_Preparation_Kit/Arrays/Snail/Snail/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snail
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> rowList = new List<int[]>();
+            while (true)
+            {
+                string s = Console.ReadLine();
+                if (s == String.Empty)
+                    break;
+                else
+                {
+                    int[] row = Array.ConvertAll(s.Split(' '), Int32.Parse);
+                    rowList.Add(row);
+                }
+            }            
+            int l = rowList.Count;
+            int[][] array = new int[l][];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = rowList[i];
+            }            
+            int[] result = Snail(array);
+            foreach (int i in result)
+                Console.Write(i + " ");
+            Console.ReadLine();
+
+        }
+        public static int[] Snail(int[][] array)
+        {
+            int verticalSize = array.GetUpperBound(0) + 1;
+            int horisontalSize = array[0].GetUpperBound(0) + 1;
+            int[] result = new int[verticalSize * horisontalSize];
+            int resultId = 0;
+            int i = 0, j = 0;
+            int lowerX = -1, lowerY = -1, upperX = horisontalSize, upperY = verticalSize;
+            while (resultId < result.Length)
+            {
+                //left to right
+                for (; i < upperX; i++)
+                {
+                    result[resultId++] = array[j][i];
+                }
+                i--;
+                upperX--;
+                j++;
+
+                //top to bottom
+                for (; j < upperY; j++)
+                {
+                    result[resultId++] = array[j][i];
+                }
+                j--;
+                upperY--;
+                i--;
+
+                //right to left
+                for (; i > lowerX; i--)
+                {
+                    result[resultId++] = array[j][i];
+                }
+                i++;
+                lowerX++;
+                j--;
+
+                //bottom to top
+                lowerY++;
+                for (; j > lowerY; j--)
+                {
+                    result[resultId++] = array[j][i];
+                }
+                j++;
+                i++;
+            }
+            return result;
+        }
+    }
+}
